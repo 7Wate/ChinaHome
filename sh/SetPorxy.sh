e01307121a11f0e355c3572ac31a4a4ea61f61b9
7Wate <zhouzhongping@7wate.com>
2023-01-18 17:59:16 +0800

    add sleep 3s

--- sh/SetPorxy.sh
+++ sh/SetPorxy.sh
@@ -3,6 +3,7 @@ read -p "Please enter the proxy address(e.g 127.0.0.1:7890): " proxy_addr
 
 echo "export http_proxy=http://$proxy_addr/" >> ~/.bashrc
 echo "export https_proxy=http://$proxy_addr/" >> ~/.bashrc
+sleep 3
 source ~/.bashrc
 
 echo "Proxy has been set successfully!"

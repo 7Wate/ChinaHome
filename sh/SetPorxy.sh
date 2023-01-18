a6934457e401946fbbba7f37fd338ac4e11ccdbe
7Wate <zhouzhongping@7wate.com>
2023-01-18 17:55:54 +0800

    fix sh

--- sh/SetPorxy.sh
+++ sh/SetPorxy.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-
-if [ "$EUID" -ne 0 ]; then
-    echo "Please run as root or use sudo"
-    exit
-fi
-
 read -p "Please enter the proxy address(e.g 127.0.0.1:7890): " proxy_addr
 
 echo "export http_proxy=http://$proxy_addr/" >> ~/.bashrc

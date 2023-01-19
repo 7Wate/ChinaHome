8b51d5380da5112d01bc8f4ecc10508c1c2cd2cf
7Wate <zhouzhongping@7wate.com>
2023-01-19 08:49:36 +0800

    fix porxy&check

--- sh/check.sh
+++ sh/check.sh
@@ -13,13 +13,13 @@ else
 fi
 
 
-url1="https://9i5.top/sh/DockerInstallation.sh"
-url2="https://gitee.com/SuperManito/LinuxMirrors/raw/main/DockerInstallation.sh)"
+url3="https://9i5.top/sh/DockerInstallation.sh"
+url4="https://gitee.com/SuperManito/LinuxMirrors/raw/main/DockerInstallation.sh)"
 
-file1=$(curl -sSL $url1)
-file2=$(curl -sSL $url2)
+file3=$(curl -sSL $url3)
+file4=$(curl -sSL $url4)
 
-if [ "$file1" != "$file2" ]; then
+if [ "$file3" != "$file4" ]; then
     echo "The contents of the DockerInstallation are different, please update"
 else
     echo "The contents of the DockerInstallation are the same"
